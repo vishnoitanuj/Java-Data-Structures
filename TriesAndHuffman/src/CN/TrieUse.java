0cd1d3d53282536cad05635d44229039c2b757db
Tanuj Vishnoi <vishnoitanuj@outlook.com>
2018-05-28 20:46:21 +0530

    Trie and Huffman started

---
+++ TriesAndHuffman/src/CN/TrieUse.java
@@ -0,0 +1,14 @@
+package CN;
+
+public class TrieUse {
+	
+	public static void main(String args[])throws Exception{
+		Trie obj = new Trie();
+		obj.add("stop");
+		obj.add("andy");
+		obj.add("start");
+		System.out.println(obj.search("andy"));
+		System.out.println(obj.search("dy"));
+	}
+
+}
