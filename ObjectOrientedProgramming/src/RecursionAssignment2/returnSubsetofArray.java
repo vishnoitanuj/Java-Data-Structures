1b928222ec55f3f4d635a4781c34fa4f95ef3eb3
Tanuj Vishnoi <vishnoitanuj@outlook.com>
2019-01-29 02:29:51 +0530

    subsets recursion added

--- ObjectOrientedProgramming/src/RecursionAssignment2/returnSubsetofArray.java
+++ ObjectOrientedProgramming/src/RecursionAssignment2/returnSubsetofArray.java
@@ -3,8 +3,43 @@ package RecursionAssignment2;
 public class returnSubsetofArray {
 	
 	public static void main(String[] args) {
-		int input[] = {1,2,3,4};
+		int input[] = {1,2,3};
+		int output[][]= subsets(input);
+		for(int i=0;i<output.length;i++) {
+			for(int j=0;j<output[i].length;j++)
+				System.out.print(output[i][j]);
+			System.out.println();
+		}
+			
 		
+	}
+	
+	public static int[][] subsets(int input[]){
+		return subsets(input, 0);
+	}
+	
+	public static int[][] subsets(int input[], int startIndex){
+		if(input.length==startIndex) {
+			int out[][]=new int[1][0];
+			return out;
+		}
+		int smallOut[][] = subsets(input,startIndex+1);
+		int out[][] = new int[2*smallOut.length][];
+		int k=0;
+		for(int i=0;i<smallOut.length;i++) {
+			out[k] = new int[smallOut[i].length];
+			for(int j=0;j<smallOut[i].length;j++)
+				out[k][j] = smallOut[i][j];
+			k++;
+		}
+		for(int i=0;i<smallOut.length;i++) {
+			out[k] = new int[smallOut[i].length+1];
+			out[k][0] = input[startIndex];
+			for(int j=1;j<=smallOut[i].length;j++)
+				out[k][j] = smallOut[i][j-1];
+			k++;
+		}
+		return out;
 	}
 
 }
